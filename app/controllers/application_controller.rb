ff04c78642d0c74b3f4980546e729bfe607528ce
alevellop <alevellop@gmail.com>
2016-02-25 16:44:06 +0000

    Set up gem dependencies and run rspec generator

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
+
+#---contiunar por página 59 de Rails 4 in Action
